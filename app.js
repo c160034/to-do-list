6b94192487dc7f0c05032218464db573a6c74e21
c160034 <chiawei960229@gmail.com>
2022-10-09 18:11:09 +0800

    Add functionality to add item

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home')
 });
 
+items = ["update", "clean", "return"]
+
 app.get('/index', (req, res) => {
-    items = ["update", "clean", "return"]
     res.render('index', { items })
 });
+
+app.get('/new', (req, res) => {
+    res.render('new');
+})
+
+app.post('/index', (req, res) => {
+    console.log(req.body)
+    items.push(req.body.title)
+    res.redirect('/index')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')

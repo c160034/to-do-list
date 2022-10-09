b5eed0e8ffb769916c463350ecae39e6ead3471b
c160034 <chiawei960229@gmail.com>
2022-10-09 17:01:19 +0800

    Display dummy items

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
+app.get('/index', (req, res) => {
+    items = ["update", "clean", "return"]
+    res.render('index', { items })
+});
+
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })

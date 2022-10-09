f91c519c2c7b515ca17612a4bc90d544fe733a1e
c160034 <chiawei960229@gmail.com>
2022-10-09 21:52:15 +0800

    added edit and delete features

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')
 });
 
-items = ["update", "clean", "return"]
+items = [{id:0, title:"update", deadline: ""}, {id:1, title:"clean", deadline: ""}, {id:2, title:"return", deadline: ""}]
 
 app.get('/index', (req, res) => {
     res.render('index', { items })
@@ -19,9 +21,33 @@ app.get('/new', (req, res) => {
 })
 
 app.post('/index', (req, res) => {
-    console.log(req.body)
-    items.push(req.body.title)
+    req.body.item.id=items.length
+    items.push(req.body.item)
     res.redirect('/index')
+})
+
+app.get('/:id/edit', (req, res) => {
+    const item = items.filter(item => item.id == req.params.id)[0]
+    res.render('edit', { item } );
+})
+
+app.put('/index/:id', (req, res) => {
+    const { id } = req.params;
+    newItem = { 
+        id: parseInt(id),
+        title: req.body.item.title,
+        deadline: req.body.item.deadline
+    }
+    items.splice(id, 1, newItem);
+    res.redirect('/index')
+});
+
+app.delete('/index/:id', (req, res) => {
+    const { id } = req.params;
+    item1=items.slice(0,id);
+    item2=items.slice(parseInt(id)+1);
+    items=item1.concat(item2);
+    res.redirect('/index');
 })
 
 app.listen(3000, () => {

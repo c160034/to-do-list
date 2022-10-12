5ef3980dd819725e93f3811416a9c601dd7c226c
c160034 <chiawei960229@gmail.com>
2022-10-12 17:39:52 +0800

    updated homepage

--- public/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-// const cards = document.querySelectorAll('.card');
-// for (let card of cards){
-//     card.addEventListener('click', function(){
-//         this.classList.toggle("done");
-//         // console.log(document)
-//         // item.complete = !item.complete;
-//     })
-// }
